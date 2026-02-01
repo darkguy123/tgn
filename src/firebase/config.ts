a6de4a43a48855fffc184f78829a343722f7f4e3
cycle breeze <cyclebreeze6@gmail.com>
2026-02-01 15:35:03 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "appId": "1:920019392653:web:c180002190c1876b292ac4",
   "apiKey": "AIzaSyC37aXzWq0Qt8Ymovk3O99hBLAQj6Y0mJ4",
   "authDomain": "studio-2980512836-4cbef.firebaseapp.com",
-  "storageBucket": "studio-2980512836-4cbef.appspot.com",
+  "storageBucket": "studio-2980512836-4cbef.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "920019392653"
 };
